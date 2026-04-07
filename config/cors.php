71fb732d6499aaebdb57bd5741cc240f3b6a0a43
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2026-04-07 15:13:55 +0800

    t

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     // During mobile debugging it's helpful to allow the device origin.
     // For quick testing we allow all origins and disable credentialed requests.
     // IMPORTANT: revert to stricter origins and enable credentials when done.
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://agriculture-murex-omega.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
